63fe8fef325dc7e229ad2d790db99f8ac4267b1f
jfvrn <joe.favre@heig-vd.ch>
2025-05-06 09:05:58 +0200

    Created Requests

---
+++ app/Http/Requests/CreateChoixRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateChoixRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'passage_id' => ['required', 'integer', 'exists:passages,id'],
+            'texte' => ['required', 'string', 'max:255'],
+            'suivant_id' => ['nullable', 'integer', 'exists:passages,id'],
+        ];
+    }
+}
